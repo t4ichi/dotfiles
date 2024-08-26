25df6baac96ef79050f47c0af380e3087c6bc8c5
t4ichi <taichinoheya020316@icloud.com>
2024-08-27 00:19:14 +0900

    add tree.lua treesitter.lua

---
+++ .config/nvim/lua/plugins/tree.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          follow_current_file = true,
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      window = {
+        mappings = {
+          ["o"] = "open",
+        },
+      },
+    })
+  end,
+}
