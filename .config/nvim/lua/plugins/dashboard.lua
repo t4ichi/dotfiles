6b0707b79292142b3403fb541121839e341c41c2
t4ichi <taichinoheya020316@icloud.com>
2024-09-08 12:15:42 +0900

    fix

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -2,19 +2,33 @@ return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
-    local logo = [[
-   ██╗████████╗ ██████╗ ██╗   ██╗    ████████╗ █████╗ ██╗ ██████╗██╗  ██╗██╗
-   ██║╚══██╔══╝██╔═══██╗██║   ██║    ╚══██╔══╝██╔══██╗██║██╔════╝██║  ██║██║
-   ██║   ██║   ██║   ██║██║   ██║       ██║   ███████║██║██║     ███████║██║
-   ██║   ██║   ██║   ██║██║   ██║       ██║   ██╔══██║██║██║     ██╔══██║██║
-   ██║   ██║   ╚██████╔╝╚██████╔╝       ██║   ██║  ██║██║╚██████╗██║  ██║██║
-   ╚═╝   ╚═╝    ╚═════╝  ╚═════╝        ╚═╝   ╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝
-    ]]
+    --  local logo = [[
+    -- ██╗████████╗ ██████╗ ██╗   ██╗    ████████╗ █████╗ ██╗ ██████╗██╗  ██╗██╗
+    -- ██║╚══██╔══╝██╔═══██╗██║   ██║    ╚══██╔══╝██╔══██╗██║██╔════╝██║  ██║██║
+    -- ██║   ██║   ██║   ██║██║   ██║       ██║   ███████║██║██║     ███████║██║
+    -- ██║   ██║   ██║   ██║██║   ██║       ██║   ██╔══██║██║██║     ██╔══██║██║
+    -- ██║   ██║   ╚██████╔╝╚██████╔╝       ██║   ██║  ██║██║╚██████╗██║  ██║██║
+    -- ╚═╝   ╚═╝    ╚═════╝  ╚═════╝        ╚═╝   ╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝
+    --  ]]
 
     require('dashboard').setup {
+      theme = 'hyper',
       config = {
-        header = vim.split(logo, "\n"),  -- Display the logo
-        -- You can add other configurations like center, footer, etc. here
+        -- header = vim.split(logo, "\n"),  -- Display the logo
+        week_header = {
+          enable=true  --boolean use a week header
+        },
+        shortcut = {
+          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+        },
       }
     }
   end,
