67ca8fcf8eb6f50bf7ffecee5492ceb9335b432f
t4ichi <taichinoheya020316@icloud.com>
2024-09-16 17:00:14 +0900

    fix

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -43,5 +43,14 @@ return {
     config = function()
       require('treesj').setup({--[[ your config ]]})
     end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
   }
 }
