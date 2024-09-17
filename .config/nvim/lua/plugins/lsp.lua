ce7acd5984c865ab5c93679c50a0a71437811468
t4ichi <taichinoheya020316@icloud.com>
2024-09-18 00:13:10 +0900

    fix

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -16,11 +16,59 @@ return {
       local lspconfig = require("lspconfig")
       require("mason-lspconfig").setup_handlers({
         function(server_name)
-          lspconfig[server_name].setup({})
+          lspconfig[server_name].setup({
+            -- Add this configuration to enable formatting
+            on_attach = function(client, bufnr)
+              if client.supports_method("textDocument/formatting") then
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                  buffer = bufnr,
+                  callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                  end,
+                })
+              end
+            end,
+            -- You can add more server-specific settings here
+          })
         end,
       })
+      -- Configure diagnostics
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          focusable = true,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
 
+      local function copy_diagnostic_message()
+        local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+        if #diagnostics > 0 then
+          local message = diagnostics[1].message
+          vim.fn.setreg("+", message) -- Copy to system clipboard
+          vim.fn.setreg('"', message) -- Copy to unnamed register
+          print("Diagnostic message copied to clipboard")
+        else
+          print("No diagnostic message at cursor position")
+        end
+      end
+
+      -- Diagnostic keymaps using <Leader>
+      vim.keymap.set('n', '<Leader>lf', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+      vim.keymap.set('n', '<Leader>lp', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+      vim.keymap.set('n', '<Leader>ln', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+      vim.keymap.set('n', '<Leader>ll', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+      vim.keymap.set('n', '<Leader>lc', copy_diagnostic_message, { desc = "Copy diagnostic message" })
       vim.api.nvim_create_autocmd("LspAttach", {
+
         callback = function()
           local opts = { noremap = true, silent = true }
           vim.keymap.set('n', 'gk', '<cmd>lua vim.lsp.buf.hover()<CR>')
