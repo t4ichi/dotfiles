16376934c1275a4efa9fff6b45d6af647a2fb7ce
t4ichi <taichinoheya020316@icloud.com>
2024-09-29 12:20:45 +0900

    fix: keymao

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,19 +5,15 @@ vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
 -- cursor
 vim.api.nvim_set_keymap("", "k", "gk", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "j", "gj", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "K", "10k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "J", "10j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "H", "b", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "L", "w", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "H", "0", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "L", "$", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("", "<C-h>", "0", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "<C-l>", "$", { noremap = true, silent = true })
 
 -- edit
 vim.api.nvim_set_keymap("", "x", '"_x', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<D>", "dd", { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("n", "<D>", "_D", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>dd", 'dd', { noremap = true, silent = true })
 -- buffer
 vim.api.nvim_set_keymap("", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>[", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,18 +1,18 @@
 return {
-	-- {
-	--   'nvim-telescope/telescope.nvim',
-	--   tag = '0.1.8',
-	--   dependencies = { 'nvim-lua/plenary.nvim' },
-	--   config = function()
-	--     local opts = { noremap = true, silent = true }
-	--
-	--     -- Find files including hidden ones
-	--     vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files hidden=true<cr>', opts)
-	--
-	--     -- List open buffers
-	--     vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<cr>', opts)
-	--   end
-	-- },
+	{
+	  'nvim-telescope/telescope.nvim',
+	  tag = '0.1.8',
+	  dependencies = { 'nvim-lua/plenary.nvim' },
+	  config = function()
+	    local opts = { noremap = true, silent = true }
+
+	    -- Find files including hidden ones
+	    vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files hidden=true<cr>', opts)
+
+	    -- List open buffers
+	    vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<cr>', opts)
+	  end
+	},
 	{
 		"akinsho/toggleterm.nvim",
 		config = function()
