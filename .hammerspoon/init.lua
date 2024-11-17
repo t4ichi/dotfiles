e28035f70238d9bdd6ab09d9b719a77dbf5c2fb7
t4ichi <taichinoheya020316@icloud.com>
2024-11-17 15:30:19 +0900

    add:アプリケーションのパスを動的に取得するようにtoggleApp関数を修正し、複数のアプリケーションを追加

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,19 +1,33 @@
 function toggleApp(appName, key)
   hs.hotkey.bind({"option"}, key, function()
     local app = hs.application.get(appName)
+    local appPaths = {
+      "/Applications/" .. appName .. ".app",
+      "/System/Applications/" .. appName .. ".app",
+      "/System/Library/CoreServices/" .. appName .. ".app"
+    }
+    local appPath = nil
+    for _, path in ipairs(appPaths) do
+      if hs.fs.attributes(path) then
+        appPath = path
+        break
+      end
+    end
     if app == nil then
-      hs.application.launchOrFocus("/Applications/" .. appName .. ".app")
+      hs.application.launchOrFocus(appPath)
     elseif app:isFrontmost() then
       app:hide()
     else
-      hs.application.launchOrFocus("/Applications/" .. appName .. ".app")
+      hs.application.launchOrFocus(appPath)
     end
   end)
 end
 
 toggleApp("Google Chrome", "j")
 toggleApp("WezTerm", "k")
--- toggleApp("Slack", "l")
+toggleApp("Slack", "l")
 toggleApp("Visual Studio Code", "h")
 toggleApp("Finder", "f")
 toggleApp("Notion", "n")
+toggleApp("Music", "m")
+toggleApp("Postman", "p")
