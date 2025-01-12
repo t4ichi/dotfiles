5a628c36b5a5effd305aa04426632945639fa585
t4ichi <taichinoheya020316@icloud.com>
2025-01-13 00:04:39 +0900

    feat: 新しいプラグインの追加と設定の改善

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -1,15 +1,23 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"tyru/open-browser.vim",
+		keys = {
+			{ "gx", "<Plug>(openbrowser-smart-search)", desc = "Open URL under cursor" },
+			{ "gx", "<Plug>(openbrowser-smart-search)", desc = "Search selected text", mode = "v" },
+		},
+	},
 }
