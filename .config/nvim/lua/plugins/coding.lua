02f29b593950c86642a538d802e1c1ae38d1f71d
t4ichi <taichinoheya020316@icloud.com>
2025-01-12 22:55:47 +0900

    style: コードの可読性向上のため、インデントを修正

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,55 +1,64 @@
 return {
-  {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
-    opts = {
-      modes = { insert = true, command = true, terminal = false },
-      -- skip autopair when next character is one of these
-      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-      -- skip autopair when the cursor is inside these treesitter nodes
-      skip_ts = { "string" },
-      -- skip autopair when next character is closing pair
-      -- and there are more closing pairs than opening pairs
-      skip_unbalanced = true,
-      -- better deal with markdown code blocks
-      markdown = true,
-    },
-    config = function(_, opts)
-      require('mini.pairs').setup(opts)
-    end,
-  },
-  {
-    "echasnovski/mini.comment",
-    version = false,
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("mini.comment").setup {
-        mappings = {
-          comment = "<leader>/",
-          comment_line = "<leader>/",
-          comment_visual = "<leader>/",
-          textobject = "<leader>/",
-        },
-      }
-    end,
-  },
-  {
-    "smjonas/inc-rename.nvim",
-    config = function()
-      require("inc_rename").setup()
-      vim.keymap.set("n", "<leader>rn", ":IncRename ")
-    end,
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require('nvim-ts-autotag').setup({
-        filetypes = { "html", "xml", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue" },
-        skip_tags = {
-          'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'slot',
-          'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr','menuitem'
-        }
-      })
-    end,
-  }
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+		config = function(_, opts)
+			require("mini.pairs").setup(opts)
+		end,
+	},
+	{
+		"echasnovski/mini.comment",
+		version = false,
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("mini.comment").setup({
+				mappings = {
+					comment = "<leader>/",
+					comment_line = "<leader>/",
+					comment_visual = "<leader>/",
+					textobject = "<leader>/",
+				},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				filetypes = { "html", "xml", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue" },
+				skip_tags = {
+					"area",
+					"base",
+					"br",
+					"col",
+					"command",
+					"embed",
+					"hr",
+					"img",
+					"slot",
+					"input",
+					"keygen",
+					"link",
+					"meta",
+					"param",
+					"source",
+					"track",
+					"wbr",
+					"menuitem",
+				},
+			})
+		end,
+	},
 }
