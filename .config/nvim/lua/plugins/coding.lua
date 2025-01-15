e8776d027b998172631cee066fdded380ffbf2f5
t4ichi <taichinoheya020316@icloud.com>
2025-01-15 23:01:07 +0900

    feat: コメント機能の改善のため、Comment.nvimとnvim-ts-context-commentstringを追加

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -11,19 +11,6 @@ return {
 		},
 	},
 	{
-		"echasnovski/mini.comment",
-		version = false,
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			mappings = {
-				comment = "<leader>/",
-				comment_line = "<leader>/",
-				comment_visual = "<leader>/",
-				textobject = "<leader>/",
-			},
-		},
-	},
-	{
 		"windwp/nvim-ts-autotag",
 		event = "VeryLazy",
 		opts = {
@@ -49,5 +36,28 @@ return {
 				"menuitem",
 			},
 		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		lazy = true,
+		keys = {
+			{ "gc", mode = { "n", "x" } },
+			{ "gb", mode = { "n", "x" } },
+			{ "gcc", mode = "n" },
+		},
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+			})
+		end,
 	},
 }
