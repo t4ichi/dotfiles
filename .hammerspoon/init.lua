6990f16e4971ea3121c30d1e7c9b7abb793605b7
t4ichi <taichinoheya020316@icloud.com>
2025-02-08 15:03:29 +0900

    style: コードのインデントを統一するため、toggleApp関数のインデントを修正

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,32 +1,31 @@
 function toggleApp(appName, key)
-  hs.hotkey.bind({"option"}, key, function()
-    local app = hs.application.get(appName)
-    local appPaths = {
-      "/Applications/" .. appName .. ".app",
-      "/System/Applications/" .. appName .. ".app",
-      "/System/Library/CoreServices/" .. appName .. ".app"
-    }
-    local appPath = nil
-    for _, path in ipairs(appPaths) do
-      if hs.fs.attributes(path) then
-        appPath = path
-        break
-      end
-    end
-    if app == nil then
-      hs.application.launchOrFocus(appPath)
-    elseif app:isFrontmost() then
-      app:hide()
-    else
-      hs.application.launchOrFocus(appPath)
-    end
-  end)
+	hs.hotkey.bind({ "option" }, key, function()
+		local app = hs.application.get(appName)
+		local appPaths = {
+			"/Applications/" .. appName .. ".app",
+			"/System/Applications/" .. appName .. ".app",
+			"/System/Library/CoreServices/" .. appName .. ".app",
+		}
+		local appPath = nil
+		for _, path in ipairs(appPaths) do
+			if hs.fs.attributes(path) then
+				appPath = path
+				break
+			end
+		end
+		if app == nil then
+			hs.application.launchOrFocus(appPath)
+		elseif app:isFrontmost() then
+			app:hide()
+		else
+			hs.application.launchOrFocus(appPath)
+		end
+	end)
 end
 
 toggleApp("Arc", "j")
 toggleApp("Ghostty", "k")
 toggleApp("Slack", "l")
-toggleApp("Visual Studio Code", "h")
 toggleApp("Finder", "f")
 toggleApp("Notion", "n")
 toggleApp("Music", "m")
