59732e86d15154b8b21da9f6b9776fc29aaf1543
t4ichi <taichinoheya020316@icloud.com>
2025-02-15 20:50:43 +0900

    fix: VSCode設定・キーバインドのパス指定方法を修正し、拡張機能インストール処理を追加

--- .bin/vscode.sh
+++ .bin/vscode.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Define the source and target paths
-declare -A FILES_TO_LINK=(
-  ["$HOME/dotfiles/vscode/settings.json"]="$HOME/Library/Application Support/Code/User/settings.json"
-  ["$HOME/dotfiles/vscode/keybindings.json"]="$HOME/Library/Application Support/Code/User/keybindings.json"
-)
+# Define the source and target paths for settings and keybindings
+SETTINGS_SOURCE_PATH="$HOME/dotfiles/vscode/settings.json"
+SETTINGS_TARGET_PATH="$HOME/Library/Application Support/Code/User/settings.json"
+KEYBINDINGS_SOURCE_PATH="$HOME/dotfiles/vscode/keybindings.json"
+KEYBINDINGS_TARGET_PATH="$HOME/Library/Application Support/Code/User/keybindings.json"
 
-# Create directories and symbolic links
-for SOURCE in "${!FILES_TO_LINK[@]}"; do
-  TARGET="${FILES_TO_LINK[$SOURCE]}"
-  mkdir -p "$(dirname "$TARGET")"
-  ln -sf "$SOURCE" "$TARGET"
-done
+# Create symbolic links for VSCode settings and keybindings
+ln -sf "$SETTINGS_SOURCE_PATH" "$SETTINGS_TARGET_PATH"
+ln -sf "$KEYBINDINGS_SOURCE_PATH" "$KEYBINDINGS_TARGET_PATH"
 
 echo "VSCode Symbolic links created successfully."
+
+# Install VSCode extensions
+echo "Installing extensions..."
+code --install-extension vscodevim.vim
+
+echo "VSCode extensions installed successfully."
