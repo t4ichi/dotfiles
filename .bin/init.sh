ea6e47d446cf0842d87f9faa9e9e4a5eb7991e76
t4ichi <taichinoheya020316@icloud.com>
2025-02-15 19:42:50 +0900

    feat: oh-my-zshとtmux plugin managerのインストールを追加

--- .bin/init.sh
+++ .bin/init.sh
@@ -21,3 +21,18 @@ else
   echo "Homebrew is already installed."
 fi
 
+# Check if oh-my-zsh is installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "oh-my-zsh not found. Installing oh-my-zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  echo "oh-my-zsh is already installed."
+
+
+# Check if tmux plugin manager is installed
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  echo "tmux plugin manager not found. Installing tmux plugin manager..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+  echo "tmux plugin manager is already installed."
+fi
