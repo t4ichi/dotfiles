38a7d36a783b30f9911e3ea46a30d29e03d674c5
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-03-30 07:56:45 +0900

    refactor: UIの簡素化と効率化のため、通知システムを変更
    - noice.nvimをsnacks.nvimに置き換え
    - telescope-ui-selectを削除
    - 不要なプラグイン（dashboard-nvim, lazygit.nvim, swagger-ui-watcher）を削除
    - 依存関係（nui.nvim, nvim-notify）を整理

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -10,7 +10,26 @@ return {
           { section = "startup" },
         },
       },
-    }
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    event = "VeryLazy",
+    opts = {
+      notifier = {
+        timeout = 1000,
+      },
+      input = {}
+    },
+    keys = {
+      {
+        "<leader>nh",
+        function()
+          require("snacks.notifier").show_history()
+        end,
+        desc = "Notify",
+      },
+    },
   },
   {
     "romgrk/barbar.nvim",
@@ -28,73 +47,6 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "BufReadPost",
     opts = {},
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-    opts = {
-      stages = "static",
-      presets = {
-        lsp_doc_border = true,
-        command_palette = true,
-        long_message_to_split = true,
-      },
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-        hover = {
-          enabled = true,
-          border = {
-            style = "rounded",
-            padding = { 0, 1 },
-          },
-        },
-        signature = {
-          enabled = true,
-          border = {
-            style = "rounded",
-            padding = { 0, 1 },
-          },
-        },
-      },
-    },
-    keys = {
-      {
-        "<leader>nh",
-        function()
-          require("noice").cmd("history")
-        end,
-        desc = "Noice History",
-      },
-
-      {
-        "<leader>nt",
-        function()
-          require("noice").cmd("pick")
-        end,
-        desc = "Noice Picker (Telescope/FzfLua)",
-      },
-    },
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
   },
   {
     "echasnovski/mini.cursorword",
