f1a2cea1e3bf1cfd2077bb9247d716f72ea6cbb1
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-06-21 00:14:16 +0900

    refactor: 既存のnvimをlazyvimに置き換え

---
+++ .config/nvim.bak/lua/config/autocmds.lua
@@ -0,0 +1,1 @@
+vim.cmd("autocmd FileType help wincmd L")
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,27 +1,3 @@
-vim.g.mapleader = " "
-
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
-
--- cursor
-vim.api.nvim_set_keymap("", "k", "gk", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "j", "gj", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "H", "0", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "L", "$", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "K", "10k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "J", "10j", { noremap = true, silent = true })
-
--- edit
-vim.api.nvim_set_keymap("", "x", '"_x', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "c", '"_c', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
-
--- buffer
-vim.api.nvim_set_keymap("", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>[", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>]", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>d", "<Cmd>BufferClose<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>D", "<Cmd>BufferCloseAllButCurrent<CR>", { noremap = true, silent = true })
-
--- window
-vim.api.nvim_set_keymap("n", "<C-w>|", ":vsplit<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-w>-", ":split<CR>", { noremap = true, silent = true })
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
