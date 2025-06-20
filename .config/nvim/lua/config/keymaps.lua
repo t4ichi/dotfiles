e4cd1ec27a4e0a9e83d48066f92983af9422fe34
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-06-21 01:15:11 +0900

    feat: keymapを追加

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Set leader key
+vim.g.mapleader = " "
+
+-- Insert mode escape
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+-- Cursor movement
+vim.keymap.set("", "k", "gk", { noremap = true, silent = true })
+vim.keymap.set("", "j", "gj", { noremap = true, silent = true })
+vim.keymap.set("", "K", "10k", { noremap = true, silent = true })
+vim.keymap.set("", "J", "10j", { noremap = true, silent = true })
+
+-- Edit operations
+vim.keymap.set("", "x", '"_x', { noremap = true, silent = true })
+vim.keymap.set("n", "c", '"_c', { noremap = true, silent = true })
+vim.keymap.set("n", "dd", '"_dd', { noremap = true, silent = true })
+
+-- Buffer operations
+vim.keymap.set("", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<Leader>[", "<cmd>bprevious<cr>", { noremap = true, silent = true, desc = "Prev buffer" })
+-- vim.keymap.set("n", "<Leader>]", "<cmd>bnext<cr>", { noremap = true, silent = true, desc = "Next buffer" })
+vim.keymap.set("n", "<Leader>d", "<cmd>bdelete<cr>", { noremap = true, silent = true, desc = "Delete buffer" })
+
+-- Window operations
+vim.keymap.set("n", "<C-w>|", "<cmd>vsplit<cr>", { noremap = true, silent = true, desc = "Split window vertically" })
+vim.keymap.set("n", "<C-w>-", "<cmd>split<cr>", { noremap = true, silent = true, desc = "Split window horizontally" })
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "K", false }
+    end,
+  },
+}
