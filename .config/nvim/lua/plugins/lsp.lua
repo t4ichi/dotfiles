c9d18e7df3e529e9a9d295c7e1acef9d20e7d965
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-06-21 16:13:27 +0900

    feat: lspのgk,gaコマンドを追加

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,6 +4,8 @@ return {
     opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "gk", vim.lsp.buf.hover, desc = "Hover" }
+      keys[#keys + 1] = { "ga", vim.lsp.buf.code_action, desc = "Code Action" }
     end,
   },
 }
