c6cce392698956479ccd9849bc60f8bddc1a8253
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-11-22 18:30:07 +0900

    chore: CopilotChat.nvimとcopilot.luaのバージョン更新、およびLSPキーマップ設定の構造変更

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,17 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "K", false }
-      keys[#keys + 1] = { "gk", vim.lsp.buf.hover, desc = "Hover" }
-      keys[#keys + 1] = { "ga", vim.lsp.buf.code_action, desc = "Code Action" }
-      keys[#keys + 1] = { "ge", vim.diagnostic.open_float, desc = "Show Diagnostic" }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "K", false },
+            { "gk", vim.lsp.buf.hover, desc = "Hover" },
+            { "ga", vim.lsp.buf.code_action, desc = "Code Action" },
+            { "ge", vim.diagnostic.open_float, desc = "Show Diagnostic" },
+          },
+        },
+      },
+    },
   },
 }
